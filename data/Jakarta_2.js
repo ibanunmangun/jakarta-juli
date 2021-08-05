e81e3004d8fb15bacd0270c952b8a3ac6d02526b
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-05 18:33:04 +0800

    Add files via upload

---
+++ data/Jakarta_2.js
@@ -0,0 +1,21 @@
+var json_Jakarta_2 = {
+"type": "FeatureCollection",
+"name": "Jakarta_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Jakarta Planetarium", "desc": "Jakarta Planetarium, Jl. Cikini Raya No.73, RT.8\/RW.2, Cikini, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10330, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8388532, -6.1900819 ] } },
+{ "type": "Feature", "properties": { "name": "MOJA Museum", "desc": "MOJA Museum, Gelora Bung Karno Sports Complex, Main Stadium, Jl. Gerbang Pemuda No.1, RW.3, Gelora, Daerah Khusus Ibukota Jakarta 10270, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8034472, -6.2174531 ] } },
+{ "type": "Feature", "properties": { "name": "Cafe Strawberry", "desc": "Cafe Strawberry, 1, Jl. Tanjung Duren Raya No.10, RT.1\/RW.6, Tj. Duren Utara, Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11470, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.7835053, -6.1795554 ] } },
+{ "type": "Feature", "properties": { "name": "dia.lo.gue", "desc": "Dia.lo.gue, Jl. Kemang Sel. No.99A, RT.1\/RW.2, Bangka, Mampang Prapatan, South Jakarta City, Jakarta 12730, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8163211, -6.2730077 ] } },
+{ "type": "Feature", "properties": { "name": "Museum Layang-layang Indonesia", "desc": "Museum Layang-layang Indonesia, Jl. H. Kamang No.38, RW.10, Pd. Labu, Kec. Cilandak, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12450, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.790548, -6.3080282 ] } },
+{ "type": "Feature", "properties": { "name": "Setu Babakan", "desc": "Setu Babakan, Srengseng Sawah, Jagakarsa, South Jakarta City, Jakarta, Indonesia", "X": null, "Y": null, "info": "point of interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8238889, -6.3416667 ] } },
+{ "type": "Feature", "properties": { "name": "Polda Metro Jaya", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.812446783189003, -6.22254095490919 ] } },
+{ "type": "Feature", "properties": { "name": "RSCM", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.846879525516997, -6.19710963094344 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Pondok Labu", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.795795794829999, -6.31351459262494 ] } },
+{ "type": "Feature", "properties": { "name": "Jakarta Medical Centre", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.830014574885993, -6.27137160281384 ] } },
+{ "type": "Feature", "properties": { "name": "Mushola Nurul Iman", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.789829368520003, -6.30816200528067 ] } },
+{ "type": "Feature", "properties": { "name": "Pertamina Lebak Bulus", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.780834365044001, -6.30906540529452 ] } },
+{ "type": "Feature", "properties": { "name": "Pertamina Senayan", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.797417142713002, -6.22909965393655 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Al-Bina GBK", "desc": null, "X": null, "Y": null, "info": "public facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.802383505940995, -6.22137646418973 ] } }
+]
+}
